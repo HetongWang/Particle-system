39ef605ff4b69b246d301836f6e661ee66db4e9b
王同和 <Hetong583@qq.com>
2014-03-13 20:27:54 +0800

    complete basic frame

--- test.js
+++ test.js
@@ -1,8 +1,46 @@
+// creat snake Object
+var snake = {
+	hx : 10,
+	hy : 10,
+	len : 1
+};
+var apple = {
+	x : 5,
+	y : 5
+}
+var speed = 500;
+
 function config() {
+	setTimeout(controller(),speed);
+	// g.fillRect(Unit, Unit, Unit, Unit); //test code
+}
+
+function controller() {
+
+}
+
+function view() {
 	var canvas = document.getElementById("canvas"),
 			g = canvas.getContext("2d");
 	var Unit = 25;
-	canvas.width = 800;
-	canvas.height = 600;
-	g.fillRect(Unit, Unit, Unit, Unit); //test code
+	canvas.width = 800; //32 colum
+	canvas.height = 600; //24 row 
+	// transform coordinate to px
+	function toRealxy(n) {
+		return n * Unit;
+	}
+
+	// draw Snakes
+	function drawSnakes() {
+
+	}
+
+	// draw Apple
+	function creatApple() {
+		function random(n) {
+			return Math.floor(Math.random() * n + 1);
+		}
+		var x = random(canvas.width / Unit),
+				y = random(canvas.height / Unit);
+	}
 }
